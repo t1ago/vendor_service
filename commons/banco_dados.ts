a499a46b4a1e5ad06a14cc30a49d72f3ff44a505
Tiago H. Piantavinha <tiago.henrip@hotmail.com>
2025-05-29 16:53:32 -0300

    ajustes de banco de dados

--- commons/banco_dados.ts
+++ commons/banco_dados.ts
@@ -1,10 +1,10 @@
- import { Client } from "pg";
+import { Client } from "pg";
 
-export const db_cliente = () => { 
+export const db_cliente = () => {
     const cliente = new Client({
         connectionString: process.env.CONNECTION_STRING,
         ssl: true
     });
-    
+
     return cliente
 }
