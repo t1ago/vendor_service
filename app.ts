34df2cfd6ed3027e5f882d74af477b3a25562cdf
MigueLendo <miguelsaccoo@gmail.com>
2025-07-07 21:28:57 -0300

    Merge branch 'main' into funcionalidade/fornecedor_miguel

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import rotasCategorias from "./modulos/produtos/categorias/categorias_rotas"
 import rotasCores from "./modulos/produtos/cores/cores_rotas"
 import gruporota from "./modulos/produtos/grupo/grupo_rotas"
+import rotasfornecedor_miguel from "./modulos/produtos/fornecedor_miguel/fornecedor_miguel_rotas"
 
 /** Constantes do Servidor*/
 const app = express()
@@ -25,7 +26,7 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/categorias", rotasCategorias)
 app.use("/cores", rotasCores)
 app.use("/grupos", gruporota)
-
+app.use("/fornecedor/miguel", rotasfornecedor_miguel)
 /** Inicia o Servidor */
 app.listen(port, () => {
   console.log(`Vendor Service (estudos) listen on PORT: ${port}`)
