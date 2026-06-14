5a3b66f07b48d7c63ef35e6cef026e47e40e4eca
Tiago H. PIantavinha <tiago.henrip@hotmail.com>
2026-06-14 12:28:21 -0300

    correção de autenticador

--- src/modulos/miguel/moedas/moedas_rotas.ts
+++ src/modulos/miguel/moedas/moedas_rotas.ts
@@ -1,12 +1,12 @@
-import express from "express"
-import { buscar, delet, inserir, update } from "./moedas_controlador"
-import { authenticadorInteceptador } from "../../../utils/miguel/utils"
+import express from 'express';
+import { autenticadorInterceptador } from '../../../utils/utils';
+import { buscar, delet, inserir, update } from './moedas_controlador';
 
-const miguel_moedas = express.Router()
+const miguel_moedas = express.Router();
 
-miguel_moedas.get("/", authenticadorInteceptador, buscar)
-miguel_moedas.post("/", authenticadorInteceptador, inserir)
-miguel_moedas.put("/:id", authenticadorInteceptador, update)
-miguel_moedas.delete("/:id", authenticadorInteceptador, delet)
+miguel_moedas.get('/', autenticadorInterceptador, buscar);
+miguel_moedas.post('/', autenticadorInterceptador, inserir);
+miguel_moedas.put('/:id', autenticadorInterceptador, update);
+miguel_moedas.delete('/:id', autenticadorInterceptador, delet);
 
-export default miguel_moedas
+export default miguel_moedas;
